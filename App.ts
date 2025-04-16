37762dc667e05b8b9d3fba81b379598247fce678
Victor Lin <13424970+victorlin@users.noreply.github.com>
2025-04-15 23:23:03 -0700

    repurpose to google sandbox

--- App.ts
+++ App.ts
@@ -1,24 +1,53 @@
-import {app, BrowserWindow, globalShortcut, shell} from 'electron'
+import {app, BrowserWindow, Menu, MenuItem, shell} from 'electron'
 import * as path from 'path';
 
-function createWindow(url: string) {
-    console.log(`Opening URL in a new window: ${url}`)
-    
-    const window = new BrowserWindow()
-    window.maximize()
-    window.loadURL(url)
+const menu = new Menu()
+menu.append(new MenuItem({
+    label: 'Electron',
+    submenu: [
+        {
+            role: 'fileMenu',
+        },
+        {
+            role: 'help',
+            label: 'Open Gmail',
+            accelerator: process.platform === 'darwin' ? 'Cmd+M' : 'Ctrl+M',
+            click: () => { createWindow('https://mail.google.com') }
+        },
+        {
+            role: 'help',
+            label: 'Open Youtube Studio',
+            accelerator: process.platform === 'darwin' ? 'Cmd+Y' : 'Ctrl+Y',
+            click: () => { createWindow('https://studio.youtube.com') }
+        },
+    ]
+}))
 
-    window.webContents.setWindowOpenHandler(({url}) => {
-        if (url.includes('google.com') && !url.startsWith('https://www.google.com/url')) {
-            createWindow(url)
-            return { action: 'deny' }
-        } else {
-            console.log(`Opening URL in default web browser: ${url}`)
-            shell.openExternal(url)
-                .then(() => {})
-            return { action: 'deny' }
-        }
-    })
+Menu.setApplicationMenu(menu)
+
+function createWindow(url?: string) {
+    const window = new BrowserWindow()
+
+    if (url === undefined) {
+        console.log(`Opening an empty window.`)
+        window.loadFile(path.join(__dirname, 'new-window.html'))
+    }
+    else {
+        console.log(`Opening URL in a new window: ${url}`)
+        window.loadURL(url)
+        window.maximize()
+        window.webContents.setWindowOpenHandler(({url}) => {
+            if (url.includes('google.com') && !url.startsWith('https://www.google.com/url')) {
+                createWindow(url)
+                return { action: 'deny' }
+            } else {
+                console.log(`Opening URL in default web browser: ${url}`)
+                shell.openExternal(url)
+                    .then(() => {})
+                return { action: 'deny' }
+            }
+        })
+    }
 
     return window
 }
@@ -27,18 +56,6 @@ app.on('window-all-closed', () => {
     app.quit()
 })
 
-function openNewUrlWindow() {
-    console.log(`Opening an empty window.`)
-    const window = new BrowserWindow()
-    window.maximize()
-    window.loadFile(path.join(__dirname, 'new-window.html'))
-    window.once('ready-to-show', () => {
-        window.show()
-        window.webContents.focus()
-    })
-}
-
 app.whenReady().then(() => {
-    createWindow('https://mail.google.com')
-    globalShortcut.register('Command+N', openNewUrlWindow)
+    createWindow()
 })
